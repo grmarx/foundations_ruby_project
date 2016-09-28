15807cf52d7582fd79e68df9960e442cb7c1a061
Gabrielle Marx <gmarx@stripe.com>
2016-09-27 20:09:16 -0700

    Revert "adding file4"
    This reverts commit 657e5476b5b36167d5dade33085572eeac142adc.

---
+++ data_types2.rb
@@ -0,0 +1,6 @@
+puts "hllo world"
+
+puts "a string is characters"
+puts "a boolean is a true or false statement "
+puts "a list is a set of variables or characters"
+puts "a hash is a key value pair"
