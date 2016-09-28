657e5476b5b36167d5dade33085572eeac142adc
Gabrielle Marx <gmarx@stripe.com>
2016-09-27 20:07:38 -0700

    adding file4

--- data_types2.rb
+++
@@ -1,6 +0,0 @@
-puts "hllo world"
-
-puts "a string is characters"
-puts "a boolean is a true or false statement "
-puts "a list is a set of variables or characters"
-puts "a hash is a key value pair"
