4a7783f615907bf1f023448f69e4fd13522f63c4
Gabrielle Marx <gmarx@stripe.com>
2016-09-27 20:28:15 -0700

    better file

--- better_file_name2.rb
+++ better_file_name2.rb
@@ -1,5 +1,6 @@
-puts "hllo world"
+puts "hll wrld"
 
+puts "i am editing" 
 puts "a string is characters"
 puts "a boolean is a true or false statement "
 puts "a list is a set of variables or characters"
