137c267f0be6bb53ff11929409dfe1285609247d
Hannah Pritchett <hannah@stripe.com>
2016-09-27 20:27:16 -0700

    hiiii

--- better_file_name2.rb
+++ better_file_name2.rb
@@ -1,6 +1,6 @@
-puts "hllo world"
+puts "hello world"
 
-puts "a string is characters"
+puts "a string is characters. many characters."
 puts "a boolean is a true or false statement "
 puts "a list is a set of variables or characters"
 puts "a hash is a key value pair"
